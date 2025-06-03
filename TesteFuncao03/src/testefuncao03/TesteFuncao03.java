fb0bc37266e3966312663ce5ab5e57e98ecb6652
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-03 13:57:48 +0200

    Funções 03 #Aula15

--- TesteFuncao03/src/testefuncao03/TesteFuncao03.java
+++ TesteFuncao03/src/testefuncao03/TesteFuncao03.java
@@ -15,6 +15,8 @@ public class TesteFuncao03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Vai começar a contagem!");
+        System.out.println(Operacoes.contador(1,5));
     }
     
 }
