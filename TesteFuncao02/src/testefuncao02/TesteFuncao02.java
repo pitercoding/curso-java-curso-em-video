15571c9c7bc56eadc7e67259cf841b4b6861f1bf
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-03 13:50:31 +0200

    função #Aula15

---
+++ TesteFuncao02/src/testefuncao02/TesteFuncao02.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao02;
+
+/**
+ *
+ * @author Piter
+ */
+public class TesteFuncao02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sm = soma(5,2);
+        System.out.println("A soma vale " + sm);
+    }
+    
+}
---
+++ TesteFuncao03/src/testefuncao03/TesteFuncao03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao03;
+
+/**
+ *
+ * @author Piter
+ */
+public class TesteFuncao03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
